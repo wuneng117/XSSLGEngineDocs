8cd198aae4288c3992aabf8b6fe35eb2a304ecc1
wuneng117 <540669277@qq.com>
2023-02-28 14:49:15 +0800

    111

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,12 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'myst_parser',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'myst_parser',
 ]
 
 intersphinx_mapping = {
